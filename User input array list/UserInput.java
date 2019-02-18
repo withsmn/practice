38a3dc021fdfebe580f47a764bc132a73d00276b
Smn Rahman <34040968+withsmn@users.noreply.github.com>
2019-02-18 19:22:08 +0600

    Update UserInput.java

--- User input array list/UserInput.java
+++ User input array list/UserInput.java
@@ -1,18 +1,28 @@
+package arrayinput;
+
 import java.util.Scanner;
 
 public class UserInput {
-
-	public static void main(String[] args) {
-		Scanner scString = new Scanner (System.in);
-		Scanner scInt = new Scanner (System.in);
-		System.out.println("Enter your name: ");
-		String name = scString.nextLine();
-		System.out.println("Enter your age: ");
-		int age = scInt.nextInt();
+	public static void main(String[] args ) {
+		int length;
+		Scanner input = new Scanner(System.in);
+		System.out.println("How many friends name that you are gonna enter?");
+		length = input.nextInt();
 		
-		System.out.println("Name: "+name+"\n"+"Age: "+age);
+		String[] names = new String[length];
+		for (int c = 0; c < length; c++ ) {
+			System.out.println("Enter the name of friend" +(c+1));
+			names[c] = input.next();
+			
+			
+		}
 		
-
+		input.close();
+		
+		System.out.println("Your friends are");
+		for (int c = 0; c < length; c++ ) {
+			System.out.println(names[c]);
+		}
 	}
 
 }
